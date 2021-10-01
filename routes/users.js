28608c2f78bb8f36ade368129633d11ddfddf319
caarlosdamian <dapecarlos@gmail.com>
2021-10-01 17:52:44 -0500

    Feat/Add update user endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,28 @@
+const { verifytokenAndauthorization } = require("./verifyToken");
+const User = require("../models/User");
 const router = require("express").Router();
+
+
+//Update
+router.put("/:id", verifytokenAndauthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
