86bd724203a79a40906eebd5e405af01c37702a9
caarlosdamian <dapecarlos@gmail.com>
2021-10-01 18:50:05 -0500

    Feat/ Add Products endpoints

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const moongose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
-
+const productRoute = require("./routes/product");
 
 dotenv.config();
 moongose
@@ -13,6 +13,7 @@ moongose
   .catch((err) => console.log(err));
 app.use(express.json());
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 app.use("/api/auth", authRoute);
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server running");
